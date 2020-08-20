ea17f6413627f9e6282cf7007218185fecbac971
Erin Driggers <erindriggers@splicemachine.com>
2020-08-19 22:08:56 -0400

    Update the query editor and updated the README.md file

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# grafana-plugin
+# grafana
+
+## Development Setup
+
+### Quickstart
+
+Build the code
+
+```bash
+cd grafana-plugins/splice-plugin && yarn install && yarn dev
+```
+
+Start Grafana
+
+```bash
+cd ../..
+docker run -d -p 3000:3000 -v "$(pwd)"/grafana-plugins:/var/lib/grafana/plugins --name=grafana grafana/grafana:7.0.0
+```
+
+Open Browser (admin)
+```
+http://localhost:3000
+default credentails admin / admin
+```
+
+
+Restart Grafana (after making changes to code and recompiling)
+```bash
+docker stop grafana
+docker rm grafana
+```
+
+## Resources
+
+API
+https://grafana.com/docs/grafana/latest/packages_api/
--- grafana-plugins/splice-plugin/src/QueryEditor.tsx
+++ grafana-plugins/splice-plugin/src/QueryEditor.tsx
@@ -1,16 +1,49 @@
-import defaults from 'lodash/defaults';
-
 import React, { ChangeEvent, PureComponent } from 'react';
-import { LegacyForms } from '@grafana/ui';
-import { QueryEditorProps } from '@grafana/data';
-import { DataSource } from './DataSource';
-import { defaultQuery, SpliceDataSourceOptions, SpliceQuery } from './types';
+import { Select } from '@grafana/ui';
+import { SelectableValue, QueryEditorProps } from '@grafana/data';
 
-const { FormField } = LegacyForms;
+import { DataSource } from './DataSource';
+import { SpliceDataSourceOptions, SpliceQuery } from './types';
+
+const FORMAT_OPTIONS: Array<SelectableValue<string>> = [
+  { label: 'Time series', value: 'time_series' },
+  { label: 'Table', value: 'table' },
+];
 
 type Props = QueryEditorProps<DataSource, SpliceQuery, SpliceDataSourceOptions>;
 
-export class QueryEditor extends PureComponent<Props> {
+interface State {
+  formatOption: SelectableValue<string>;
+}
+
+export class QueryEditor extends PureComponent<Props, State> {
+  // Query target to be modified and used for queries
+  query: SpliceQuery;
+
+  constructor(props: Props) {
+    super(props);
+
+    // Use default query to prevent undefined input values
+    const defaultQuery: Partial<SpliceQuery> = {};
+    const query = Object.assign({}, defaultQuery, props.query);
+    this.query = query;
+
+    // Query target properties that are fully controlled inputs
+    this.state = {
+      // Select options
+      formatOption: FORMAT_OPTIONS.find(option => option.value === query.format) || FORMAT_OPTIONS[0],
+    };
+  }
+
+  onFieldChange = (query: ChangeEvent<HTMLTextAreaElement>) => {
+    this.query.queryText = query.target.value;
+  };
+
+  onFormatChange = (option: SelectableValue<string>) => {
+    this.query.format = option.value;
+    this.setState({ formatOption: option }, this.onRunQuery);
+  };
+
   onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { onChange, query } = this.props;
     onChange({ ...query, queryText: event.target.value });
@@ -23,27 +56,42 @@ export class QueryEditor extends PureComponent<Props> {
     onRunQuery();
   };
 
+  onRunQuery = () => {
+    const { query } = this;
+    this.props.onChange(query);
+    this.props.onRunQuery();
+  };
+
   render() {
-    const query = defaults(this.props.query, defaultQuery);
-    const { queryText, constant } = query;
+    const { formatOption } = this.state;
 
     return (
-      <div className="gf-form">
-        <FormField
-          width={4}
-          value={constant}
-          onChange={this.onConstantChange}
-          label="Constant"
-          type="number"
-          step="0.1"
-        />
-        <FormField
-          labelWidth={8}
-          value={queryText || ''}
-          onChange={this.onQueryTextChange}
-          label="Query Text"
-          tooltip="Not used yet"
-        />
+      <div className="gf-form-group">
+        <div className="gf-form-inline">
+          <div className="gf-form gf-form--grow">
+            <textarea
+              rows={7}
+              className="gf-form-input"
+              placeholder="Enter a splice machine query"
+              data-min-length={0}
+              data-items={100}
+              onChange={this.onFieldChange}
+            ></textarea>
+          </div>
+        </div>
+
+        <div className="gf-form-inline">
+          <div className="gf-form">
+            <div className="gf-form-label width-7">Format</div>
+            <Select
+              width={16}
+              isSearchable={false}
+              options={FORMAT_OPTIONS}
+              onChange={this.onFormatChange}
+              value={formatOption}
+            />
+          </div>
+        </div>
       </div>
     );
   }
